6ed1442dddfd27a0c82be5e7c54c2e856662f84e
Alan Kasis <alankasis@gmail.com>
2024-02-29 13:54:31 -0300

    fix: text color on dark mode

--- src/components/Bullet.jsx
+++ src/components/Bullet.jsx
@@ -6,7 +6,7 @@ const Bullet = ({ content }) => {
             <div className="bg-[#457B9D] rounded-full p-1">
                 <img className="h-6 min-w-6 invert" src="/check-mark.png" />
             </div>
-            <p className="ml-6 md:ml-10 font-bold sm:text-xl">{content}</p>
+            <p className="ml-6 md:ml-10 font-semibold sm:text-xl text-black">{content}</p>
         </div>
     )
 }
