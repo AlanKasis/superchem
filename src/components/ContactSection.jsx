8a6b1a7c1854ff19bd3962258d7252593ef8fb67
Alan Kasis <alankasis@gmail.com>
2024-03-25 18:39:15 -0300

    fix: fullname input regex / success message on email send color green

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -45,7 +45,7 @@ const ContactSection = () => {
               id="input-group-1"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5"
               placeholder="Javier Quagliano"
-              {...register("fullName", { required: true, pattern: /^[A-Za-z]+$/i })}
+              {...register("fullName", { required: true, pattern: /^[A-Za-z\s]+$/i })}
             />
           </div>
           <label htmlFor="input-group-1" className="block mb-2 text-sm font-medium text-gray-900">Email</label>
@@ -106,7 +106,7 @@ const ContactSection = () => {
             })}
           ></textarea>
           <button type="submit" className="mt-6 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Enviar</button>
-          {success && <span>{success}</span>}
+          {success && <span className="mt-6 text-[#52b963]">{success}</span>}
         </form>
         <img src="/distillation-sketch.png" className="hidden sm:block" />
       </div>
